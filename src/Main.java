add368affea0384c051dfae090d21ce7fffc248a
Gian Luca Rivera <gian.luca.99@hotmail.com>
2019-02-08 11:21:23 -0600

    Cantidad numeros

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int len = 10;
+        Scanner read = new Scanner(System.in);
+        int len = 0;
+        System.out.println("Ingrese la cantidad de números que desea generar: ");
+        len = read.nextInt();
         ArrayList<Integer> array = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i<len;)
+        for (int i = 0; i<len; i++) {
+            array.add(random.nextInt(len));
+        }
 
         //Insertion Sort
         System.out.println("INSERTION SORT");
